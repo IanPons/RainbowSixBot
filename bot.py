ff896fa2c9976113cfffe6ef4e3f31054f00fb61
Ian Pons <50560178+IanPons@users.noreply.github.com>
2020-08-11 16:48:22 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from search_player import buscar_jogador
 
 
-TOKEN = "Njk3NTA3ODgzOTQ4MDQ4NDQ0.Xo5SbA.Rbqf3ClzD9X9_emsRslf_oOzzRg"
+TOKEN = "MY TOKEN"
 
 bot = commands.Bot(command_prefix='!')
 
@@ -22,30 +22,6 @@ async def StatsPs4(ctx, nick):
 @bot.command()
 async def SatsXbox(ctx, nick):
 	await ctx.send(buscar_jogador(nick, 'xbox'))
-
-
-
-'''
-@bot.command()
-async def DanielDeKaid(ctx):
-	await ctx.send('D E R R O T A')
-
-@bot.command()
-async def PedroDeDoc(ctx):
-	await ctx.send('C O R R E')
-
-@bot.command()
-async def Jotape1(ctx):
-	await ctx.send('Planet Time')
-
-@bot.command()
-async def Jotape2(ctx):
-	await ctx.send('Fake Alibi')
-
-
-@bot.command()
-async def Lucas(ctx):
-	await ctx.send('Mr.1v1')
 
 '''
 bot.run(TOKEN)
